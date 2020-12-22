9450c68c0ecadd98e01c90b0d14404adb9772ab6
Moisés Mariano <moisesduartem@gmail.com>
2020-12-22 00:53:13 -0300

    production code

---
+++ dist/js/bundle.js
@@ -0,0 +1,1 @@
+(()=>{var e=document.querySelector("h1");document.querySelector("#colorChanger").addEventListener("click",(function(t){e.style.color="#"+(16777215*Math.random()<<0).toString(16).padStart(6,"0")}))})();
